f0140d320e4d63347e748cd4a70c88c4e69d1633
justicepelteir <justice.douglas@hey.com>
2022-06-04 14:08:26 -0700

    add foreign key to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_210334) do
+ActiveRecord::Schema.define(version: 2022_06_04_210649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,4 +32,5 @@ ActiveRecord::Schema.define(version: 2022_06_04_210334) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reviews", "products"
 end
