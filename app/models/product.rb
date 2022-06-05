76f7fd54601af60df45fc64955d7cc0c69b8ceac
justicepelteir <justice.douglas@hey.com>
2022-06-04 21:02:02 -0700

    clean up order

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,10 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
-  validates :country_of_orgin, presence: true
+  validates_length_of :name, minimum: 3, maximum: 100
   validates :cost, presence: true
   validates_numericality_of :cost, greater_than: 0, less_than_or_equal_to: 1000000
-  validates_length_of :name, minimum: 3, maximum: 100
+  validates :country_of_orgin, presence: true
 
   before_save(:titleize_product)
 
