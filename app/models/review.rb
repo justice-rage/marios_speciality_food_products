350dd7c56e3c0d9845bf5e26212edb198f10bcfb
justicepelteir <justice.douglas@hey.com>
2022-06-04 20:30:28 -0700

    add review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
     belongs_to :product
+    validates :author, presence: true
+    validates :rating, presence: true
+    validates_numericality_of :rating, only_integer: true, greater_than: 0, less_than_or_equal_to: 5
+    validates :content_body, presence: true
+    validates_length_of :content_body, minimum: 50, maximum: 250
   end
