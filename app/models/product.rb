bcd9b7bc04d0bdceb30d23c99f6f103b2d1f16f6
justicepelteir <justice.douglas@hey.com>
2022-06-04 18:51:56 -0700

    add titelize product name test - success

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,11 @@ class Product < ApplicationRecord
     validates :name, presence: true
 
     validates_length_of :name, maximum: 100
+
+    before_save(:titleize_product)
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+    end
 end
