e52929151a26890d389197602e4c8ae3e4ebd15d
justicepelteir <justice.douglas@hey.com>
2022-06-05 14:51:38 -0700

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Review.destroy_all
+
+50.times do
+    product = Product.create!(
+      name: Faker::Food.sushi,
+      cost: Faker::Number.between(from: 1, to: 100),
+      country_of_origin: Faker::Address.country
+    )
+
+    5.times do
+      Review.create!(
+        author: Faker::GreekPhilosophers.name,
+        rating: Faker::Number.between(from: 1, to: 5),
+        content_body: Faker::Lorem.paragraph_by_chars(number:50),
+        product_id: product.id
+      )
+    end
+end
+
+p "Create #{Review.count} reviews"
+p "Create #{Product.count} products"
