3624572ff6a1f3b885f38e2ae06909a54e9bd3b0
justicepelteir <justice.douglas@hey.com>
2022-06-04 18:48:53 -0700

    add titelize product name test - fail

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,9 @@ describe Product do
   it { should validate_presence_of :name }
 
   it { should validate_length_of(:name).is_at_most(100) }
+
+  it("titleizes the name of an product") do
+    product = Product.create({name: "kale", cost: 5, country_of_origin: "usa"})
+    expect(product.name()).to(eq("Kale"))
+  end
 end
