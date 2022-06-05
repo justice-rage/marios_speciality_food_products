29fdc518b1b0da356b40761d2e6d9a3fdcae06b9
justicepelteir <justice.douglas@hey.com>
2022-06-04 20:36:08 -0700

    add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,15 @@
 class Product < ApplicationRecord
   scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
 
-    has_many :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
-    validates :name, presence: true
+  validates :name, presence: true
+  validates :country_of_orgin, presence: true
+  validates :cost, presence: true
+  validates_numericality_of :cost, greater_than: 0, less_than_or_equal_to: 1000000
+  validates_length_of :name, minimum: 3, maximum: 100
 
-    validates_length_of :name, maximum: 100
-
-    before_save(:titleize_product)
+  before_save(:titleize_product)
 
   private
     def titleize_product
