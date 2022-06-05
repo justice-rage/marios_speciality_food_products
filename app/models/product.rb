14ae447ade5c63914ea415a796d3ce2b049cb25e
justicepelteir <justice.douglas@hey.com>
2022-06-05 12:48:00 -0700

    add scopes: most reviewed/usa made

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
   scope :most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :most_reviewed, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+  )}
+  scope :usa_product, -> { where(country_of_origin: "United States of America") }
 
   has_many :reviews, dependent: :destroy
 
