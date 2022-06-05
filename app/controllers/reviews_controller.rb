c323b5840a983be84971553d7a6422ad29075798
justicepelteir <justice.douglas@hey.com>
2022-06-05 11:29:45 -0700

    add flash messages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,29 +9,33 @@ class ReviewsController < ApplicationController
       @product = Product.find(params[:product_id])
       @review = @product.reviews.new(review_params)
       if @review.save
+        flash[:notice] = "Review successfully added!"
         redirect_to product_path(@product)
       else
+        flash[:alert] = "Error detected - review not created. Please try again."
         render :new
       end
     end
-  
+    
     def show
       @product = Product.find(params[:product_id])
       @review = Review.find(params[:id])
       render :show
     end
-
+    
     def edit
       @product = Product.find(params[:product_id])
       @review = Review.find(params[:id])
       render :edit
     end
-
+    
     def update
       @review = Review.find(params[:id])
       if @review.update(review_params)
+        flash[:notice] = "Review successfully added!"
         redirect_to product_path(@review.product)
       else
+        flash[:alert] = "Error detected - review not updated. Please try again."
         @product = Product.find(params[:product_id])
         render :edit
       end
