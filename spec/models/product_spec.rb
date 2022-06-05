d7feb6c2b95bbc34249236db96351ebcac2879c9
justicepelteir <justice.douglas@hey.com>
2022-06-05 16:44:33 -0700

    tests passing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
-
   it { should validate_presence_of :name }
-
   it { should validate_length_of(:name).is_at_most(100) }
+  it { should validate_length_of(:name).is_at_least(3) }
+  it { should validate_presence_of :cost }
+  it { should validate_numericality_of(:cost).is_greater_than(0) }
+  it { should validate_numericality_of(:cost).is_less_than_or_equal_to(1000000) }
 
   it("titleizes the name of an product") do
     product = Product.create({name: "kale", cost: 5, country_of_origin: "usa"})
