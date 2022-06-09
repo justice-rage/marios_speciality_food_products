4c0d1ce120f7964fbef2cbd417bc611214c1637a
justicepelteir <justice.douglas@hey.com>
2022-06-08 19:42:29 -0700

    add landing page logic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -57,6 +57,20 @@ class ProductsController < ApplicationController
         redirect_to products_path
     end
 
+    def home
+      case
+      when params[:sort] == "most_recent"
+        @products = Product.most_recent
+      when params[:sort] == "most_reviewed"
+        @products = Product.most_reviewed
+      when params[:sort] == "usa_product"
+        @products = Product.usa_product
+      else
+        @products = Product.all
+      end
+      render :home
+    end
+
     private
     def product_params
       params.require(:product).permit(:name, :cost, :country_of_origin)
