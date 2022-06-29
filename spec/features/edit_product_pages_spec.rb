c7250d9e90e4539c80682af7f76a2bb0efaa462e
justicepelteir <justice.douglas@hey.com>
2022-06-28 18:31:13 -0700

    add passing tests

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 describe "edit product lifecycle" do
+  before(:each) do
+    visit new_user_registration_path
+    click_on "Sign up!"
+    fill_in "user_email", :with => "admin@epicodus.com"
+    fill_in "user_password", :with => "epicodus123"
+    fill_in "user_password_confirmation", :with => "epicodus123"
+    click_on "Sign up"
+    dumby_admin = User.where(:email => "admin@epicodus.com").update(:admin => true)
+  end
+
   it "edits a product" do
     product = Product.create(:name => 'Salmon', :cost => '12', :country_of_origin => "Canada")
     visit edit_product_path(product)
